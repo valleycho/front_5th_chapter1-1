518d1c73192d95cc278d45fc934771346f42878b
valleycho <qkrtjdtlr02@naver.com>
2025-03-26 00:05:49 +0900

    배포시도(설정변경 6)

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    base: "/front_5th_chapter1-1/",
+    base: "./",
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+          hash: "./index.hash.html",
+        },
+      },
+    },
   },
 });
