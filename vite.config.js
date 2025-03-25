efe6890f2bef41c917793a626e179fc9718c2d45
valleycho <qkrtjdtlr02@naver.com>
2025-03-26 01:30:26 +0900

    배포 시도(설정변경 20)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/front_5th_chapter1-1/",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
   },
-  base: "/front_5th_chapter1-1/",
   build: {
     outDir: "dist",
     rollupOptions: {
