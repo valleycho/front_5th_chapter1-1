1ecf7a77b5c9c5c86faa1b74e4ba10e367f643b5
valleycho <qkrtjdtlr02@naver.com>
2025-03-25 18:48:20 +0900

    배포 됐는데도 페이지 안보이는게 vitest base 설정 안해서인지 테스트

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    base: "",
   },
 });
