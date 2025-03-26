f03d6ae6f8d32b8e72c0d0b4924edfe6afa94fe8
valleycho <qkrtjdtlr02@naver.com>
2025-03-26 10:32:56 +0900

    배포 시도(설정변경 31)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig(({ mode }) => {
   return {
     base: mode === "production" ? "/front_5th_chapter1-1/" : "/",
+    define: {
+      "import.meta.env.MODE": JSON.stringify(mode),
+    },
     build: {
       outDir: "dist",
     },
